1ef49d605f5627380597110e2e0f51f181d985a1
Gabriel <gborbamarques@gmail.com>
2021-03-19 12:34:41 -0300

    API Consumindo os dados da api ->Api_do_zero_com_React<-

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './api';
+import "./app.css";
+//import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+  state = {
+    artigos: [],
+  }
+  async componentDidMount() {
+    const response = await api.get('http://localhost:8080');
+
+    this.setState({ artigos: response.data })
+  }
+  render() {
+  
+    const { artigos } = this.state;
+    console.log(artigos);
+    return ( 
+      <div className="geral">  
+        <h1 className="font-geral">
+          
+          Sala de Troféus do Sport Club Internacional
+        </h1>
+        <ul className="fundo-api">
+          
+          {artigos.map(artigo => (
+            <p key={artigo._id}>
+              <hr></hr>
+              <div className="caixaNome">
+                <h2 className="titulo">
+                Título: {artigo.titulo}
+              </h2>
+              </div>
+              <p className="conteudo">
+                <strong>Descrição do Título: {artigo.conteudo}</strong>
+              </p>
+              <div className="imagens">
+                <img className="img" src={artigo.imagem} />
+                <img className="gif" src={artigo.gif}/>
+               
+              </div>
+              <strong>
+                <p className="ano">
+                Ano de conquista: {artigo.ano}
+              </p>
+              </strong>
+            </p>
+          ))}
+        </ul>
+      </div>
+
+    );
+  };
+};
+
 
 export default App;
